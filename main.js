9db26840d4562f465d794ed3e0296863504c05cc
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-15 15:56:38 -0500

    Create 16 tiles

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 const container = document.querySelector('#container');
+let sketchpad = [];
+for (let i = 0; i < 16; i++) {
+    sketchpad[i] = document.createElement('div');
+    sketchpad[i].classList.add('tile');
+    container.appendChild(sketchpad[i]);
+}
