26c2d808d94a2138afa9b08c40f900992bbd4f92
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-15 16:03:22 -0500

    Create 16x16 grid

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const container = document.querySelector('#container');
 let sketchpad = [];
+let index = 0;
+
 for (let i = 0; i < 16; i++) {
-    sketchpad[i] = document.createElement('div');
-    sketchpad[i].classList.add('tile');
-    container.appendChild(sketchpad[i]);
+    for (let j = 0; j < 16; j++) {
+        index = i * 16 + j;
+        sketchpad[index] = document.createElement('div');
+        sketchpad[index].classList.add('tile');
+        container.appendChild(sketchpad[index]);
+
+        if (index % 16 === 0) {
+            sketchpad[index].classList.add('clear');
+        }
+    }
 }
