217b5fe6fa72eb372495f1513d27fb163dc44aeb
Ricky McCallum <rickymccallum87@gmail.com>
2019-01-16 09:07:30 -0500

    New grid button

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 const container = document.querySelector('#container');
+const resetButton = document.querySelector('button');
 let sketchpad = [];
 let index = 0;
+let length;
 
-// Create grid
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        index = i * 16 + j;
-        sketchpad[index] = document.createElement('div');
-        sketchpad[index].classList.add('tile');
-        container.appendChild(sketchpad[index]);
+createGrid();
 
-        if (index % 16 === 0) {
-            sketchpad[index].classList.add('clear');
-        }
+function clearGrid() {
+    for (let i = 0; i < sketchpad.length; i++) {
+        container.removeChild(sketchpad[i]);
     }
 }
 
-// Draw on hover
-for (let i = 0; i < sketchpad.length; i++) {
-    sketchpad[i].addEventListener('mouseover', (e) => {
-        e.target.classList.add('color');
-    });
+resetButton.addEventListener('click', () => {
+    clearGrid();
+    length = prompt('How many squares per side?');
+    createGrid(length);
+});
+
+// Create grid
+function createGrid(length = 16) {
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            index = i * length + j;
+            sketchpad[index] = document.createElement('div');
+            sketchpad[index].classList.add('tile');
+            container.appendChild(sketchpad[index]);
+
+            if (index % 16 === 0) {
+                sketchpad[index].classList.add('clear');
+            }
+        }
+    }
+
+    // Draw on hover
+    for (let i = 0; i < sketchpad.length; i++) {
+        sketchpad[i].addEventListener('mouseover', (e) => {
+            e.target.classList.add('color');
+        });
+    }
 }
